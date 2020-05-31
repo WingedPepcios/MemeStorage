47dfc178554adb9e6d762d98d9c2a394fe7b4a92
WingedPepcios <piskapiotr@winged.pl>
2020-05-31 21:34:44 +0200

    GA set user id

--- client/src/Utils/Analytics.js
+++ client/src/Utils/Analytics.js
@@ -5,3 +5,7 @@ ReactGA.initialize('UA-160262521-1');
 export const pageview = (location) => {
   ReactGA.pageview(location);
 };
+
+export const setUserId = (id) => {
+  ReactGA.set({ userId: id });
+};
