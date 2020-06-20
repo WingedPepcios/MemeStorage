8f2f392971efa3025a6defc99d17b3bc500df013
WingedPepcios <piskapiotr@winged.pl>
2020-06-20 14:53:05 +0200

    Add system version in footer

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -7,7 +7,8 @@ const Footer = () => (
   <footer className="container py-3 mt-5">
     <div className="d-flex align-items-center justify-content-center flex-column">
       <div className="mb-3">Copyright © <strong>winged.pl</strong> 2020</div>
-      <i>Administrator nie ponosi odpowiedzialności za treści na stronie.<br />Kontakt i informacje: <a href="mailto:kontakt@winged.pl">kontakt@winged.pl</a></i>
+      <div className="mb-3"><i>Administrator nie ponosi odpowiedzialności za treści na stronie.<br />Kontakt i informacje: <a href="mailto:kontakt@winged.pl">kontakt@winged.pl</a></i></div>
+      <div><small>Wersja 1.0.0</small></div>
     </div>
   </footer>
 );
